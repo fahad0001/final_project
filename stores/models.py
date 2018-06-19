1fe193a989b03f35a970db710ffc05c67dd42024
fahad0001 <fahad@relymer.com>
2018-06-19 19:02:45 +0500

    [migration] run migration on user model

--- stores/models.py
+++ stores/models.py
@@ -1,10 +1,1 @@
 from django.db import models
-
-from django.contrib.auth.models import User
-
-
-class Invitation(models.Model):
-    from_user = models.ForeignKey(User, on_delete=False, related_name="invitation_sent")
-    to_user = models.ForeignKey(User, on_delete=False, related_name="invitation_received")
-    message = models.CharField(max_length=300)
-    timestamp = models.DateTimeField(auto_now_add=True)
