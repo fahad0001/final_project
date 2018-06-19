053aace407f9e34a466f57100a74cf8e3aaf443c
fahad0001 <fahad@relymer.com>
2018-06-19 22:57:31 +0500

    [enhance] SignUp form + register route

--- fyp_ver_1/views.py
+++ fyp_ver_1/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import SignUpForm
+
 
 # Create your views here.
 
@@ -6,3 +9,25 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'default/index.html')
 
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            selected_opt = form.cleaned_data.get('is_buyer')
+            if selected_opt:
+                user.is_Buyer = True
+                user.is_Seller = False
+
+            else:
+                user.is_Seller = True
+                user.is_Buyer = False
+
+            user.save()
+            login(request, user)
+
+            return redirect('index')
+    else:
+        form = SignUpForm()
+    return render(request, 'default/signup.html', {'form': form})
